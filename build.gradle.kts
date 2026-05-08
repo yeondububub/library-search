b5e552deab9db6fc15118d98514904564dc142ac
ydbb <yeondububub@gmail.com>
2026-05-08 14:04:55 +0900

    멀티모듈 구성

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,35 +5,52 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "com"
-version = "0.0.1-SNAPSHOT"
-description = "library-search"
+subprojects {
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+	group = "com"
+	version = "0.0.1-SNAPSHOT"
+	description = "library-search"
+
+	java {
+		toolchain {
+			languageVersion = JavaLanguageVersion.of(17)
+		}
 	}
-}
 
-repositories {
-	mavenCentral()
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("tools.jackson.module:jackson-module-kotlin")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+	repositories {
+		mavenCentral()
 	}
-}
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+	kotlin {
+		compilerOptions {
+			freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+		}
+	}
+
+	dependencies {
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+		implementation("org.springframework.boot:spring-boot-starter-webmvc")
+		implementation("tools.jackson.module:jackson-module-kotlin")
+
+		testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+		testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+
+	tasks.getByName("bootJar") {
+		enabled = false
+	}
+
+	tasks.getByName("jar") {
+		enabled = false
+	}
 }
